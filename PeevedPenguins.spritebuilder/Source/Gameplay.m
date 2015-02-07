70c9db72e8aa82bfa0be5a28fdfc5e93016712bc
varun <varunv123@gmail.com>
2015-02-07 18:48:52 -0500

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Source/Gameplay.m
+++ PeevedPenguins.spritebuilder/Source/Gameplay.m
@@ -41,7 +41,7 @@
     [penguin.physicsBody applyForce:force];
     // ensure followed object is in visible are when starting
     self.position = ccp(0, 0);
-    CCActionFollow *follow = [CCActionFollow actionWithTarget:penguin];
+    CCActionFollow *follow = [CCActionFollow actionWithTarget:penguin worldBoundary:self.boundingBox];
     [self runAction:follow];
 }
 @end
